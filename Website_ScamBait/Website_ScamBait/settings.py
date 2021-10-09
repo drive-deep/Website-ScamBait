641de1f48b3668fa2ddce742e8be86b330986d56
prabhat.ranjan <deep_drive@yahoo.com>
2021-10-09 23:06:13 +0530

    secret key changed

--- Website_ScamBait/Website_ScamBait/settings.py
+++ Website_ScamBait/Website_ScamBait/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j4%)zgxtozys7+@e*d9%cyw%3g5n9cn7ichkfj%^t2a(erc$0f'
+SECRET_KEY = 'django-insecure-j4%)zgxtozys7+@e*d9%cyw%3g5n9cnfhfk7ichkfj%^t2a(erc$0f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
