5b3dcd6d576186d676f813156c482f570d217045
lucifer <prabhatranjan@prabhats-MacBook-Pro.local>
2021-10-10 14:00:46 +0530

    kk

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/lucifer/Desktop/Website-ScamBait/venv/bin/python3
+#!/Users/lucifer/Desktop/Website-ScamBait/venv/bin/python
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
