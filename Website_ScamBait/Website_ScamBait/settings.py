953600d197a8d58f8965cc9fecfea6e542f57ca6
drive_deep <deep_drive@yahoo.com>
2021-10-16 22:50:16 +0530

    hgfhj

--- Website_ScamBait/Website_ScamBait/settings.py
+++ Website_ScamBait/Website_ScamBait/settings.py
@@ -23,7 +23,7 @@ import django_heroku
 SECRET_KEY = 'django-insecure-j4%)zgxtozys7+@e*d9%cyw%3g5n9cnfhfk7ichkfj%^t2a(erc$0f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0']
 
